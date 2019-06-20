75b5998948ae92907bd298f056ce54da1f0d9c56
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-20 15:50:47 -0400

    Added a variable for the class name selector, and output. Also got the first index of the className selector with an index.

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // Query Selector
+// No semi colons because of Prettier auto formatting.
 console.log(document.querySelector("#task-title"))
 console.log(document.querySelector(".card-title"))
 console.log(document.querySelector("h5"))
+
+const items = document.getElementsByClassName("col")
+
+// Get elements by ClassName
+console.log(document.getElementsByClassName("col"))
+
+console.log(items[0])
