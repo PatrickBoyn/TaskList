b4f68edfba611cbbb148c26658c8a3e8f7c8358c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-20 14:45:26 -0400

    Set a bunch of useful properties.

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ document.getElementById("task-title").style.color = "white"
 // Sets the padding of the id
 document.getElementById("task-title").style.padding = "5px"
 // Sets the display to none
-document.getElementById("task-title").style.display = "none"
+// document.getElementById("task-title").style.display = "none"
+
+// Change the content
+document.getElementById("task-title").textContent = "Task List"
+document.getElementById("task-title").innerText = "My Tasks"
+// Adding HTML via javascript
+document.getElementById("task-title").innerHTML =
+  '<span style="color:blue">Task List HTML!</span>'
