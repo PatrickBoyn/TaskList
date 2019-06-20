fbe2b293cde473765b37070f30e656d1bd1f9b4a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-20 14:39:35 -0400

    Added a few more properties of get element by id.

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ console.log(document.getElementById("task-title"))
 
 // Changes the style of an id
 document.getElementById("task-title").style.background = "red"
+// Sets the color of the id
+document.getElementById("task-title").style.color = "white"
+// Sets the padding of the id
+document.getElementById("task-title").style.padding = "5px"
+// Sets the display to none
+document.getElementById("task-title").style.display = "none"
