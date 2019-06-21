fa3e84f264671ea7c9d150162197afc8130e452f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-20 22:53:07 -0400

    Added the UI variables.

---
+++ app.js
@@ -0,0 +1,8 @@
+// Define UI variables
+// No semi colons because of Prettier (Auto formatter)
+// This was done using a tutorial
+const form = document.querySelector("#task-form")
+const taskList = document.querySelector(".collection")
+const clearBtn = document.querySelector(".clear-tasks")
+const filter = document.querySelector("#filter")
+const taskInput = document.querySelector("#task")
