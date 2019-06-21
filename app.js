967ac8edb6365d8d8c41250239670d19ffb5c210
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-21 02:27:06 -0400

    The filter currently does not work. I need to see if it's the HTML or the JS.

--- app.js
+++ app.js
@@ -68,5 +68,13 @@ function clearTasks() {
 function filterTasks(e) {
   const text = e.target.value.toLowerCase()
 
-  document.querySelectorAll(".collection-item").forEach()
+  document.querySelectorALl(".collection-item").array.forEach(function() {
+    const item = task.firstChild.textContent
+
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block"
+    } else {
+      task.style.display = "none"
+    }
+  })
 }
