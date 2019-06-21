735bad0b0cf6e63a05f04a8ab1d3738d6409a333
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-21 12:29:30 -0400

    Added something to loop over. Also fixed an issue with autocomplete.

--- app.js
+++ app.js
@@ -68,9 +68,8 @@ function clearTasks() {
 function filterTasks(e) {
   const text = e.target.value.toLowerCase()
 
-  document.querySelectorALl(".collection-item").array.forEach(function() {
+  document.querySelectorAll(".collection-item").forEach(function(task) {
     const item = task.firstChild.textContent
-
     if (item.toLowerCase().indexOf(text) != -1) {
       task.style.display = "block"
     } else {
