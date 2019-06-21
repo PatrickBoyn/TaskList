6b77fee5d12d2011e9c744916b5fcbc28465184f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-06-20 22:54:43 -0400

    Added a function to load all event listeners.

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ const taskList = document.querySelector(".collection")
 const clearBtn = document.querySelector(".clear-tasks")
 const filter = document.querySelector("#filter")
 const taskInput = document.querySelector("#task")
+
+// Load all event listeners
+loadEventListeners()
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+}
